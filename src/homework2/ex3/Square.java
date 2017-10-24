e7ef540316fdda15fc3d73994569c85bd1e57236
Jay Grey <jaygrey@yandex.ru>
2017-10-24 19:06:03 +0300

    Добавлен класс Square.

---
+++ src/homework2/ex3/Square.java
@@ -0,0 +1,33 @@
+package homework2.ex3;
+
+import homework2.ex1.Shape;
+import homework2.ex2.Point;
+
+import static java.lang.Math.sqrt;
+
+public class Square extends Shape {
+
+    private Point a;
+    private Point b;
+    private Point c;
+    private Point d;
+
+    public Square(Point a, Point b, Point c, Point d) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+    }
+
+    @Override
+    public double getPerimetr() {
+        return a.length(b) + b.length(c) + c.length(d) + d.length(a);
+    }
+
+    @Override
+    public double getArea() {
+        double p = getPerimetr() / 2;
+        return sqrt((p - a.length(b)) * (p - b.length(c)) * (p - c.length(d))
+                * (p - d.length(a)));
+    }
+}
--- src/homework2/ex3/Test.java
+++ src/homework2/ex3/Test.java
@@ -2,13 +2,21 @@ package homework2.ex3;
 
 import homework2.ex2.Point;
 
-import static java.lang.StrictMath.abs;
+import static java.lang.Math.abs;
 
 public class Test {
+
+    // Допустимое отклонение
+    private static final double DELTA = 0.01;
+
     public static void main(String[] args) {
         System.out.println("test Triangle class ... "
-                + (testTriangle() ? "OK" : "FAILURE"));
+                + (testTriangle() ? "OK" : "FAIL"));
+
+        System.out.println("test Square class ... "
+                + (testSquare() ? "OK" : "FAIL"));
     }
+
 
     private static boolean checkEquals(double actual, double expected,
                                        double delta) {
@@ -18,9 +26,6 @@ public class Test {
     private static boolean testTriangle() {
         double EXPECTED_PERIMETER = 20.60;
         double EXPECTED_AREA = 17.50;
-
-        // Допустимое отклонение
-        double DELTA = 0.01;
 
         // Проверка для положительной плоскости
         Triangle t = new Triangle(new Point(0, 0), new Point(5, 0),
@@ -35,6 +40,27 @@ public class Test {
 
         boolean test2 = checkEquals(t.getPerimetr(), EXPECTED_PERIMETER, DELTA)
                 && checkEquals(t.getArea(), EXPECTED_AREA, DELTA);
+
+        return test1 && test2;
+    }
+
+    private static boolean testSquare() {
+        double EXPECTED_PERIMETER = 24.00;
+        double EXPECTED_AREA = 35.00;
+
+        // Проверка для положительной плоскости
+        Square s = new Square(new Point(0, 0), new Point(5, 0), new Point(5, 7),
+                new Point(0, 7));
+
+        boolean test1 = checkEquals(s.getPerimetr(), EXPECTED_PERIMETER, DELTA)
+                && checkEquals(s.getArea(), EXPECTED_AREA, DELTA);
+
+        // Проверка для отрицательной плоскости
+        s = new Square(new Point(-1, -5), new Point(-1, 0), new Point(-8, 0),
+                new Point(-8, -5));
+
+        boolean test2 = checkEquals(s.getPerimetr(), EXPECTED_PERIMETER, DELTA)
+                && checkEquals(s.getArea(), EXPECTED_AREA, DELTA);
 
         return test1 && test2;
     }
