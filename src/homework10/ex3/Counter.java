8c67d813a3fb21d02765c0311d0d30f117cea5d0
Jay Grey <jaygrey@yandex.ru>
2017-11-23 20:42:45 +0200

    Задание 3 лекции 10. добавлены примитивные массивы.

--- src/homework10/ex3/Counter.java
+++ src/homework10/ex3/Counter.java
@@ -19,4 +19,108 @@ public class Counter {
 
         return result;
     }
+
+    public static Map<Byte, Integer> countFrequency(byte[] array) {
+        if (array == null) {
+            return null;
+        }
+
+        Byte[] result = new Byte[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = array[i];
+        }
+
+        return countFrequency(result);
+    }
+
+    public static Map<Character, Integer> countFrequency(char[] array) {
+        if (array == null) {
+            return null;
+        }
+
+        Character[] result = new Character[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = array[i];
+        }
+
+        return countFrequency(result);
+    }
+
+    public static Map<Boolean, Integer> countFrequency(boolean[] array) {
+        if (array == null) {
+            return null;
+        }
+
+        Boolean[] result = new Boolean[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = array[i];
+        }
+
+        return countFrequency(result);
+    }
+
+    public static Map<Short, Integer> countFrequency(short[] array) {
+        if (array == null) {
+            return null;
+        }
+
+        Short[] result = new Short[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = array[i];
+        }
+
+        return countFrequency(result);
+    }
+
+    public static Map<Integer, Integer> countFrequency(int[] array) {
+        if (array == null) {
+            return null;
+        }
+
+        Integer[] result = new Integer[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = array[i];
+        }
+
+        return countFrequency(result);
+    }
+
+    public static Map<Long, Integer> countFrequency(long[] array) {
+        if (array == null) {
+            return null;
+        }
+
+        Long[] result = new Long[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = array[i];
+        }
+
+        return countFrequency(result);
+    }
+
+    public static Map<Float, Integer> countFrequency(float[] array) {
+        if (array == null) {
+            return null;
+        }
+
+        Float[] result = new Float[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = array[i];
+        }
+
+        return countFrequency(result);
+    }
+
+    public static Map<Double, Integer> countFrequency(double[] array) {
+        if (array == null) {
+            return null;
+        }
+
+        Double[] result = new Double[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = array[i];
+        }
+
+        return countFrequency(result);
+    }
 }
--- test/homework10/ex1/TestHW10Ex3.java
+++ test/homework10/ex1/TestHW10Ex3.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.assertNull;
 public class TestHW10Ex3 {
     @Test
     public void nullTest() {
-        assertNull(Counter.countFrequency(null));
+        assertNull(Counter.countFrequency((Object[]) null));
     }
 
     @Test
@@ -41,5 +41,68 @@ public class TestHW10Ex3 {
         Map<String, Integer> result = Counter.countFrequency(
                 new String[]{null, null});
         assertEquals(0, result.size());
+    }
+
+    @Test
+    public void testPrimitiveByteArray() {
+        Map<Byte, Integer> result = Counter.countFrequency(new byte[]{0, 1, 0});
+        assertEquals(2, result.size());
+        assertEquals(2, (int) result.get((byte) 0));
+    }
+
+    @Test
+    public void testPrimitiveShortArray() {
+        Map<Short, Integer> result = Counter.countFrequency(
+                new short[]{0, 1, 0});
+        assertEquals(2, result.size());
+        assertEquals(2, (int) result.get((short) 0));
+    }
+
+    @Test
+    public void testPrimitiveIntegerArray() {
+        Map<Integer, Integer> result = Counter.countFrequency(
+                new int[]{0, 1, 0});
+        assertEquals(2, result.size());
+        assertEquals(2, (int) result.get(0));
+    }
+
+    @Test
+    public void testPrimitiveLongArray() {
+        Map<Long, Integer> result = Counter.countFrequency(
+                new long[]{0, 1, 0});
+        assertEquals(2, result.size());
+        assertEquals(2, (int) result.get((long) 0));
+    }
+
+    @Test
+    public void testPrimitiveFloatArray() {
+        Map<Float, Integer> result = Counter.countFrequency(
+                new float[]{0.0f, 1.0f, 0.0f});
+        assertEquals(2, result.size());
+        assertEquals(2, (int) result.get((float) 0));
+    }
+
+    @Test
+    public void testPrimitiveDoubleArray() {
+        Map<Double, Integer> result = Counter.countFrequency(
+                new double[]{0.0, 1.0, 0.0});
+        assertEquals(2, result.size());
+        assertEquals(2, (int) result.get((double) 0));
+    }
+
+    @Test
+    public void testPrimitiveBooleanArray() {
+        Map<Boolean, Integer> result = Counter.countFrequency(
+                new boolean[]{false, true, true});
+        assertEquals(2, result.size());
+        assertEquals(2, (int) result.get(true));
+    }
+
+    @Test
+    public void testPrimitiveCharacterArray() {
+        Map<Character, Integer> result = Counter.countFrequency(
+                new char[]{'a', 'b', 'a'});
+        assertEquals(2, result.size());
+        assertEquals(2, (int) result.get('a'));
     }
 }
